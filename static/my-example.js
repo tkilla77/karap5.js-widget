ddb3792abb1cde4ba92b1b0af141097387b700e2
Tom Hofmann <tom@scheidweg.net>
2024-06-25 10:21:27 +0200

    better example script

--- static/my-example.js
+++ static/my-example.js
@@ -1,7 +1,15 @@
 // This is an example of an external file that can be loaded
 // into the widget via the `src` attribute.
-
-function setup() {
-  createCanvas(300, 100);
-  background(255, 0, 200);
-}
+kara.move();
+kara.turnRight();
+kara.move();
+KaraWorld.create(
+  `TTTTTTTTT
+   T >     T
+   T       T
+   T  MM   T
+   T  MM   T
+   T       T
+   T       T
+   T       T
+   TTTTTTTTT`);
