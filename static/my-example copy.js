60a1b0533169a60369ef6681cf2662bc09fa9c86
Tom Hofmann <tom@scheidweg.net>
2024-06-25 14:15:16 +0200

    Updates

---
+++ static/my-example copy.js
@@ -0,0 +1,15 @@
+// This is an example of an external file that can be loaded
+// into the widget via the `src` attribute.
+kara.move();
+kara.turnRight();
+kara.move();
+KaraWorld.create(
+  `TTTTTTTTT
+   T >     T
+   T       T
+   T  MM   T
+   T  MM   T
+   T       T
+   T       T
+   T       T
+   TTTTTTTTT`);
