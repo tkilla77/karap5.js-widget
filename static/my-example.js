fb551a101b86dbee5155d261c68989873558c1c6
Tom Hofmann <tom@scheidweg.net>
2024-06-25 22:15:03 +0200

    Updates

--- static/my-example.js
+++ static/my-example.js
@@ -1,15 +1,1 @@
-// This is an example of an external file that can be loaded
-// into the widget via the `src` attribute.
-kara.move();
-kara.turnRight();
-kara.move();
-KaraWorld.create(
-  `TTTTTTTTT
-   T >     T
-   T       T
-   T  MM   T
-   T  MM   T
-   T       T
-   T       T
-   T       T
-   TTTTTTTTT`);
+kara.move(),kara.turnRight(),kara.move(),KaraWorld.create("TTTTTTTTT\n   T >     T\n   T       T\n   T  MM   T\n   T  MM   T\n   T       T\n   T       T\n   T       T\n   TTTTTTTTT");
